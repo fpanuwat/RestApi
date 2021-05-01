ae9e5b9216c687b60a77d17e9d42a2cd26daecb6
fpanuwat <47840915+fpanuwat@users.noreply.github.com>
2021-05-01 13:25:45 +0700

    commit

--- routes/chCar/index.js
+++ routes/chCar/index.js
@@ -8,16 +8,7 @@ router.get('/:id',async(req,res)=>{
         const id = req.params.id;
         //const id = '1607741653';
         let connection = await mysql.createConnection(config.database)
-        /*let connection2 = await mysql.createPool(config.database);
-        connection2.getConnection((err,con)=>{
-            if(err) throw err;
-            console.log('connect as id :' + con.threadId);
-            con.query('SELECT * FROM ', (err,rows)=>{
-                con.release();
-                if (err) throw err;
-                console.log('the data from table are: '+rows);
-            });
-        });*/
+        
         let sql = 'select * from car where car_number = ?'
         const [data] = await connection.query(sql,id)
         res.status(200).send(data)
